58fe2b9c6c36a86d45f9dfd653bd00e59cb6c8ca
CheungXianjie <xianjie.zhang@zebra-c.com>
2022-07-07 11:54:55 +0800

    HOT TEST COMMIT

--- origin/src/main/java/com/android/origin/rests/glide/GlideExtend.kt
+++ origin/src/main/java/com/android/origin/rests/glide/GlideExtend.kt
@@ -1,5 +1,6 @@
 package com.android.origin.rests.glide
 
+import android.content.Context
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
@@ -8,35 +9,22 @@ import java.lang.RuntimeException
 /**
 Created by Zebra-RD张先杰 on 2022年5月31日11:36:20
 
-Description:
+Description:简化Glide
  */
-class ZGlide {
+fun Context.glide(init:GlideExtend.()->Unit){
+        val glide = GlideExtend.create()
+        init.invoke(glide)
+}
+class GlideExtend private constructor(){
     companion object {
-        private var mDefaultImage: Drawable? = null
-
+        private val instant by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) { GlideExtend() }
+        fun create() = instant
         fun ImageView.load(url: String, defaultImage: Drawable? = null) {
             Glide.with(this).load(when {
                 url.contains("http") -> url
                 defaultImage != null -> defaultImage
-                mDefaultImage != null -> mDefaultImage
                 else -> throw RuntimeException("图片路径不合法，并未设置占位图")
             }).into(this)
         }
-    }
-
-    /**
-     * 设置默认的占位图
-     * @param defaultImage Drawable对象
-     */
-    fun setDefaultImage(defaultImage: Drawable?) {
-        mDefaultImage = defaultImage
-    }
-
-    /**
-     * 获取当前占位图
-     * @return 当前占位图Drawable对象
-     */
-    fun getDefaultImage(): Drawable? {
-        return mDefaultImage
     }
 }
